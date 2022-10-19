fe641bd40a6c25cd9ce3a09e732629a6504ab0f1
CTO <thepropertymanager2020@gmail.com>
2022-10-20 02:51:14 +0800

    added the code on returning and non-returning methods

--- Midterm/Activities/CalculateMain.java
+++ Midterm/Activities/CalculateMain.java
@@ -1,5 +1,16 @@
 public class CalculateMain {
     public static void main(String[] args) {
 
+        int x=1;
+        int y=1;
+
+        // CalculateMethod.addNumVoid(x, y);
+
+        int z = CalculateMethod.addNumNonVoid(x, y);
+
+        System.out.println(z+1);
+
+
+
     }
 }
--- Midterm/Activities/CalculateMethod.java
+++ Midterm/Activities/CalculateMethod.java
@@ -1,3 +1,14 @@
 class CalculateMethod {
-  
+
+  public static void addNumVoid(int x, int y)
+  {
+    int z = x + y;
+
+    System.out.print(z);
+  }
+
+  public static int addNumNonVoid(int a, int b)
+  {
+    return a + b;
+  }
 }
